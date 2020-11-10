2cb185449a1bd1c73fd0cb5be28ef026a45ccb2d
Sharon.T <sharon.librae@gmail.com>
2020-11-10 18:34:38 +0800

    start shell

---
+++ start.sh
@@ -0,0 +1,11 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2513
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 HelveticaNeue;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab560
+\pard\pardeftab560\slleading20\partightenfactor0
+
+\f0\fs24 \cf0 #!/bin/bash\
+cd /home/ec2-user/Cloudwatch-Batch-Alarm/\
+python3 createAlarm.py}
